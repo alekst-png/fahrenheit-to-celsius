64c8138fbd8d417d428ce738a0a7ef1dcb9036f6
Alexander Tarling <tarlinga@cardiff.ac.uk>
2023-07-14 17:50:43 +0100

    implemented calculation method

---
+++ fahrenheitToCelsius/FahrenheitToCelsius.cs
@@ -0,0 +1,11 @@
+﻿namespace fahrenheitToCelsius;
+
+public class FahrenheitToCelsius
+{
+    // method to convert value
+    public double ConvertF(double fahrenheit)
+    {
+        double celsius = ((fahrenheit - 32) * 5) / 9;
+        return celsius;
+    }
+}
--- fahrenheitToCelsius/Program.cs
+++ fahrenheitToCelsius/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using fahrenheitToCelsius;
+
+// enter fahrenheit
+Console.WriteLine("Enter fahrenheit value: ");
+string inputValue = Console.ReadLine();
+double value = System.Convert.ToDouble(inputValue);
+
+// use method to output celsius
+FahrenheitToCelsius conv = new FahrenheitToCelsius();
+Console.WriteLine("Your value is: " + conv.ConvertF(value) + " celsius.");
